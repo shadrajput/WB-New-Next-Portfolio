9eb5fc28f92bdf18e211af5ba03c9144089aa260
shadrajput <shadrajput6@gmail.com>
2024-02-17 18:25:50 +0530

    Wellbenix 2.0

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,19 +9,6 @@ export default function AboutUs() {
 
         <div className=' p-2 lg:p-5 rounded-lg md:w-[35%] relative shadow-xl bg-gradient-to-r from-[#117bfc3e] to-[#db82fc58]'>
           <Tilt className="Tilt" options={{ max: 25 }}>
-            <div className='bg-[#b47cfd] rounded-full absolute flex justify-center items-center -top-10 z-50 -left-7 lg:-left-10 h-16 w-16 sm:h-20 lg:h-28 sm:w-20 lg:w-28 border-2 border-white '>
-              <svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <path id="circlePath" d="M 60,60 m -50,0 a 50,50 0 1,0 100,0 a 50,50 0 1,0 -100,0" fill="transparent" />
-                </defs>
-                <text fontSize="16" fontFamily="Arial" fill="white" letterSpacing="3.5">
-                  <textPath href="#circlePath" stroke="white" strokeWidth="1" strokeLinejoin="round">Premium Support & Services</textPath>
-                </text>
-              </svg>
-              <div className='border-2 border-white w-10 h-10 sm:w-12 sm:h-12 lg:h-[70px]  lg:w-[70px] absolute rounded-full'>
-
-              </div>
-            </div>
             <div className='mt-1 rounded-lg p-2'>
               <Image src={wb} alt="fess manager logo" className='rounded-lg' />
             </div>
